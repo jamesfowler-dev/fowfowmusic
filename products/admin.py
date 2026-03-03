079194d0ea33faedb9c829a917b834246cec4c37
jamesfowler-dev <james_fowler21@hotmail.com>
2026-03-03 14:23:14 +0000

    add product model and register to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product_type', 'price', 'is_available')
+    list_filter = ('product_type', 'is_available', 'category')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ('is_available', 'price')
--- products/models.py
+++ products/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    PRODUCT_TYPES = [
+        ('music', 'Music'),
+        ('preset', 'Preset Pack'),
+        ('tuition', 'Tuition'),
+    ]
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    product_type = models.CharField(max_length=20, choices=PRODUCT_TYPES)
+    image = models.ImageField(upload_to='product_images/') 
+    file = models.FileField(upload_to='product_files/', blank=True)
+    soundcloud_url = models.URLField(blank=True)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
