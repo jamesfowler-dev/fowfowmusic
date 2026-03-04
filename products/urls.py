c237f6a086a0ca3575af24553d18cc4fe9692bbc
jamesfowler-dev <james_fowler21@hotmail.com>
2026-03-04 16:17:26 +0000

    add views and urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home_page_view
+from .views import home_view, product_list_view, product_detail_view
 
 urlpatterns = [
-    path("", home_page_view),
+    path("", home_view, name="home"),
+    path('products/', product_list_view, name='product_list'),
+    path('products/<slug:slug>/', product_detail_view, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
-def home_page_view(request):  
-    return HttpResponse("Hello, World!")
+# Home Page
+def home_view(request):
+    featured_products = Product.objects.filter(is_available=True)[:6]
+    return render(request, 'products/home.html', {
+        'featured_products': featured_products,
+    })
 
+
+# Product List page
+def product_list_view(request):
+    product_type = request.GET.get('type')
+    products = Product.objects.filter(is_available=True)
+
+    type_titles = {
+        'music': 'Music',
+        'preset': 'Preset Packs',
+        'tuition': 'Tuition',
+    }
+
+    if product_type in type_titles:
+        products = products.filter(product_type=product_type)
+        page_title = type_titles[product_type]
+    else:
+        page_title = 'All Products'
+
+    return render(request, 'products/product_list.html', {
+        'products': products,
+        'page_title': page_title,
+    })
+
+
+# Product Detail page
+def product_detail_view(request, slug):
+    product = get_object_or_404(Product, slug=slug, is_available=True)
+    return render(request, 'products/product_detail.html', {
+        'product': product,
+    })
